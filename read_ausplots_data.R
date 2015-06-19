bcb8a1b08d2a8f24c3beae5b035bb42724be25f0
Greg Guerin <gregguerin@WC02H306WDHJV.ad.adelaide.edu.au>
2015-06-19 14:59:40 +0930

    Add extraction code file

---
+++ read_ausplots_data.R
@@ -0,0 +1,358 @@
+#Script for extracting specific AusPlots data and writing to file. Needs to be tailored to the exact data requirements. Result is sites summary and individual files for each plot for each data type (e.g. vegetation point intercept hits, soil bulk density...)
+#
+#Authors:
+#Andrew Tokmakoff (modifications Greg Guerin)
+
+#setwd("")
+
+
+library(RPostgreSQL)
+drv <- dbDriver("PostgreSQL")
+con <- dbConnect(drv, user="xxxx", password="xxxx", host="xxxx", dbname="xxxx")
+
+# Get all sites and their top-level site data
+rs <- dbSendQuery(con,"SELECT 
+  site_location.site_location_name, 
+  site_location.established_date, 
+  site_location.description, 
+  site_location.bioregion_name, 
+  site_location.landform_pattern, 
+  site_location.landform_element, 
+  site_location.site_slope, 
+  site_location.site_aspect, 
+  site_location.comments, 
+  site_location.outcrop_lithology, 
+  site_location.other_outcrop_lithology, 
+  site_location.plot_dimensions, 
+  site_location_visit.visit_start_date, 
+  site_location_visit.visit_end_date, 
+  site_location_visit.visit_notes, 
+  site_location_visit.location_description, 
+  site_location_visit.erosion_type, 
+  site_location_visit.erosion_abundance, 
+  site_location_visit.erosion_state, 
+  site_location_visit.microrelief, 
+  site_location_visit.drainage_type, 
+  site_location_visit.disturbance, 
+  site_location_visit.climatic_condition, 
+  site_location_visit.vegetation_condition, 
+  site_location_visit.observer_veg, 
+  site_location_visit.observer_soil, 
+  site_location_visit.described_by, 
+  site_location_visit.pit_marker_easting, 
+  site_location_visit.pit_marker_northing, 
+  site_location_visit.pit_marker_mga_zones, 
+  site_location_visit.pit_marker_datum, 
+  site_location_visit.pit_marker_location_method, 
+  site_location_visit.soil_observation_type, 
+  site_location_visit.a_s_c, 
+  site_location.plot_is_100m_by_100m, 
+  site_location.plot_is_aligned_to_grid, 
+  site_location.plot_is_permanently_marked,
+  site_location_point.latitude, 
+  site_location_point.longitude, 
+  site_location_point.point 
+FROM 
+  public.site_location, 
+  public.site_location_visit,
+  public.site_location_point
+WHERE 
+  site_location_point.point = 'SW' AND
+  site_location_point.site_location_id = site_location.site_location_id AND
+  site_location_visit.site_location_id = site_location.site_location_id;
+")
+
+df <- fetch(rs,n=-1)
+fileName <- "./extractedData/site/sitesAndDescriptions.csv"
+if (file.exists(fileName)) file.remove(fileName)
+write.csv(df, file = fileName)
+dbClearResult(rs)
+
+
+
+# Get all structural summaries
+summaries <- dbSendQuery(con,"SELECT 
+  site_location.site_location_name, 
+  structural_summary.phenology_comment, 
+  structural_summary.upper_1_dominant, 
+  structural_summary.upper_2_dominant, 
+  structural_summary.upper_3_dominant, 
+  structural_summary.mid_1_dominant, 
+  structural_summary.mid_2_dominant, 
+  structural_summary.mid_3_dominant, 
+  structural_summary.ground_1_dominant, 
+  structural_summary.ground_2_dominant, 
+  structural_summary.ground_3_dominant, 
+  structural_summary.description, 
+  structural_summary.mass_flowering_event
+FROM 
+  public.site_location, 
+  public.site_location_visit, 
+  public.structural_summary
+WHERE 
+  site_location_visit.site_location_id = site_location.site_location_id AND
+  structural_summary.site_location_visit_id = site_location_visit.site_location_visit_id;
+")
+
+summariesData <- fetch(summaries,n=-1)
+fileName <- "./extractedData/site/structuralSummaries.csv"
+if (file.exists(fileName)) file.remove(fileName)
+write.csv(summariesData, file = fileName)
+dbClearResult(summaries)
+
+
+
+
+
+getSoilsForAllSites <- function(row, con) {
+  siteNames <- row[1]
+  #print(attributes(siteName))
+  #siteName <- siteNames[1,]
+  #print(siteNames)
+
+  print('iterating over all sites for soils..\n')
+  for (i in 1:nrow(siteNames)) {
+    siteName <- siteNames[i,]
+    print(siteName)
+
+
+
+    soilsRS <- dbSendQuery(con,paste(
+      "SELECT 
+          site_location.site_location_name, 
+          soil_bulk_density.sample_id, 
+          soil_bulk_density.paper_bag_weight, 
+          soil_bulk_density.oven_dried_weight_in_bag, 
+          soil_bulk_density.ring_weight, 
+          soil_bulk_density.gravel_weight, 
+          soil_bulk_density.ring_volume, 
+          soil_bulk_density.gravel_volume, 
+          soil_bulk_density.fine_earth_weight_in_bag, 
+          soil_bulk_density.fine_earth_weight, 
+          soil_bulk_density.fine_earth_volume, 
+          soil_bulk_density.fine_earth_bulk_density, 
+          soil_bulk_density.gravel_bulk_density
+        FROM 
+          public.site_location, 
+          public.site_location_visit, 
+          public.soil_bulk_density
+        WHERE 
+          site_location.site_location_name = '", siteName, "' AND
+          site_location_visit.site_location_id = site_location.site_location_id AND
+          soil_bulk_density.site_location_visit_id = site_location_visit.site_location_visit_id;
+      ", sep = "")
+    )
+
+    soil <- fetch(soilsRS,n=-1)
+    fileName <- paste("./extractedData/soils/soilBulkDensity-", siteName, ".csv", sep = "")
+    if (file.exists(fileName)) file.remove(fileName)
+    write.csv(soil, file = fileName)
+    dbClearResult(soilsRS)
+
+
+    soilsRS <- dbSendQuery(con,paste(
+      "SELECT  
+        soil_characterisation.upper_depth, 
+        soil_characterisation.lower_depth, 
+        soil_characterisation.horizon, 
+        soil_characterisation.texture_grade, 
+        soil_characterisation.texture_qualifier, 
+        soil_characterisation.texture_modifier, 
+        soil_characterisation.colour_when_moist, 
+        soil_characterisation.colour_when_dry, 
+        site_location.site_location_name, 
+        soil_characterisation.mottles_colour, 
+        soil_characterisation.mottles_abundance, 
+        soil_characterisation.mottles_size, 
+        soil_characterisation.segregations_abundance, 
+        soil_characterisation.segregations_size, 
+        soil_characterisation.segregations_nature, 
+        soil_characterisation.segregations_form, 
+        soil_characterisation.comments, 
+        soil_characterisation.collected_by, 
+        soil_characterisation.smallest_size_1, 
+        soil_characterisation.smallest_size_2, 
+        soil_characterisation.effervescence, 
+        soil_characterisation.ec, 
+        soil_characterisation.ph, 
+        soil_characterisation.pedality_grade, 
+        soil_characterisation.pedality_fabric, 
+        soil_characterisation.next_size_type_2, 
+        soil_characterisation.next_size_type_1, 
+        soil_characterisation.smallest_size_type_2, 
+        soil_characterisation.smallest_size_type_1, 
+        soil_characterisation.next_size_2, 
+        soil_characterisation.next_size_1
+      FROM 
+        public.site_location, 
+        public.site_location_visit, 
+        public.soil_characterisation
+      WHERE 
+        site_location.site_location_name = '", siteName, "' AND
+        site_location_visit.site_location_id = site_location.site_location_id AND
+        soil_characterisation.site_location_visit_id = site_location_visit.site_location_visit_id;
+      ", sep = "")
+    )
+
+    soil <- fetch(soilsRS,n=-1)
+    fileName <- paste("./extractedData/soils/soilCharacterisation-", siteName, ".csv", sep = "")
+    if (file.exists(fileName)) file.remove(fileName)
+    write.csv(soil, file = fileName)
+    dbClearResult(soilsRS)
+
+
+
+    soilsRS <- dbSendQuery(con,paste(
+      "SELECT 
+        site_location.site_location_name, 
+        soil_subsite_observations.subsite_id, 
+        soil_subsite_observations.zone, 
+        soil_subsite_observations.easting, 
+        soil_subsite_observations.northing, 
+        soil_subsite_observations.ten_to_twenty_barcode, 
+        soil_subsite_observations.zero_to_ten_barcode, 
+        soil_subsite_observations.twenty_to_thirty_barcode, 
+        soil_subsite_observations.comments, 
+        soil_subsite_observations.metagenomic_barcode
+      FROM 
+        public.site_location, 
+        public.site_location_visit, 
+        public.soil_subsite_observations
+      WHERE 
+        site_location.site_location_name = '", siteName, "' AND
+        site_location_visit.site_location_id = site_location.site_location_id AND
+        soil_subsite_observations.site_location_visit_id = site_location_visit.site_location_visit_id;
+      ", sep = "")
+    )
+
+    soil <- fetch(soilsRS,n=-1)
+    fileName <- paste("./extractedData/soils/soilSubsites-", siteName, ".csv", sep = "")
+    if (file.exists(fileName)) file.remove(fileName)
+    write.csv(soil, file = fileName)
+    dbClearResult(soilsRS)
+
+
+
+
+
+
+
+
+  }
+}
+
+
+getVegForAllSites <- function(row, con) {
+  siteNames <- row[1]
+
+  print('iterating over all sites for veg..\n')
+  for (i in 1:nrow(siteNames)) {
+    siteName <- siteNames[i,]
+    print(siteName)
+
+    vegRS <- dbSendQuery(con,paste(
+      "SELECT 
+        site_location.site_location_name, 
+        veg_vouchers.veg_barcode,
+        herbarium_determination.herbarium_determination, 
+        herbarium_determination.is_uncertain_determination
+      FROM 
+        public.site_location, 
+        public.site_location_visit, 
+        public.veg_vouchers, 
+        public.herbarium_determination
+      WHERE 
+        site_location.site_location_name = '", siteName, "' AND
+        site_location_visit.site_location_id = site_location.site_location_id AND
+        veg_vouchers.site_location_visit_id = site_location_visit.site_location_visit_id AND
+        herbarium_determination.veg_barcode = veg_vouchers.veg_barcode;
+      ", sep = "")
+    )
+
+    veg <- fetch(vegRS,n=-1)
+    fileName <- paste("./extractedData/veg/vegVouchers-", siteName, ".csv", sep = "")
+    if (file.exists(fileName)) file.remove(fileName)
+    write.csv(veg, file = fileName)
+    dbClearResult(vegRS)
+
+    vegRS <- dbSendQuery(con,paste(
+      "SELECT 
+        site_location.site_location_name, 
+        point_intercept.transect,
+        point_intercept.point_number,
+        herbarium_determination.veg_barcode,
+        herbarium_determination.herbarium_determination,
+        point_intercept.substrate, 
+        point_intercept.in_canopy_sky, 
+        point_intercept.dead, 
+        point_intercept.growth_form, 
+        point_intercept.height
+      FROM 
+        public.site_location, 
+        public.site_location_visit, 
+        public.herbarium_determination, 
+        public.point_intercept
+      WHERE 
+        site_location.site_location_name = '", siteName, "' AND
+        site_location_visit.site_location_id = site_location.site_location_id AND
+        herbarium_determination.veg_barcode = point_intercept.veg_barcode AND
+        point_intercept.site_location_visit_id = site_location_visit.site_location_visit_id;
+
+      ", sep = "")
+    )
+
+    veg <- fetch(vegRS,n=-1)
+    fileName <- paste("./extractedData/veg/vegPI-", siteName, ".csv", sep = "")
+    if (file.exists(fileName)) file.remove(fileName)
+    write.csv(veg, file = fileName)
+    dbClearResult(vegRS)
+
+
+    vegRS <- dbSendQuery(con,paste(
+      "SELECT 
+        site_location.site_location_name, 
+        basal_area.point_id,
+        herbarium_determination.herbarium_determination, 
+        herbarium_determination.veg_barcode, 
+        basal_area.hits, 
+        basal_area.basal_area_factor, 
+        basal_area.basal_area
+      FROM 
+        public.site_location, 
+        public.site_location_visit, 
+        public.herbarium_determination, 
+        public.basal_area
+      WHERE 
+        site_location.site_location_name = '", siteName, "' AND
+        site_location_visit.site_location_id = site_location.site_location_id AND
+        herbarium_determination.veg_barcode = basal_area.veg_barcode AND
+        basal_area.site_location_visit_id = site_location_visit.site_location_visit_id;
+      ", sep = "")
+    )
+
+    veg <- fetch(vegRS,n=-1)
+    fileName <- paste("./extractedData/veg/vegBasal-", siteName, ".csv", sep = "")
+    if (file.exists(fileName)) file.remove(fileName)
+    write.csv(veg, file = fileName)
+    dbClearResult(vegRS)
+
+  }
+
+}
+
+getSoilsForAllSites(df[1], con)
+getVegForAllSites(df[1], con)
+
+
+
+
+
+
+dbDisconnect(con)
+dbUnloadDriver(drv)
+
+
+
+
+
+
