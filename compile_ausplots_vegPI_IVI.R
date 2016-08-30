c8e5b528da26f7235a139d95f61e1aa350662bce
Greg Guerin <gregguerin@WC02H306WDHJV.ad.adelaide.edu.au>
2016-08-30 16:41:37 +0930

    Minor edit

--- compile_ausplots_vegPI_IVI.R
+++ compile_ausplots_vegPI_IVI.R
@@ -3,10 +3,9 @@
 #Authors:
 #Greg Guerin
 
-#setwd()
 
 freq_matrix_percents <- freq_matrix*100 #freq_matrix defined in 'compile_ausplots_vegPI_FREQ.R'
-which(!colnames(freq_matrix_percents) %in% colnames(cover_matrix)) #all species names the same
+which(!colnames(freq_matrix_percents) %in% colnames(cover_matrix)) #check all species names the same
 IVI <- freq_matrix_percents + cover_matrix #can also calculate mean to get a 0-100% value
 
 #write to file
