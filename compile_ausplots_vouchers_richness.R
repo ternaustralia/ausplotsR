9d0f449b961d9f7312148f053c593e9e0e987e51
Greg Guerin <gregguerin@WC02H306WDHJV.ad.adelaide.edu.au>
2016-08-30 16:06:24 +0930

    Update outputs
    Updated to add a site_unique site/visit identifier and remove redundant
    code for calculating species richness scores.

--- compile_ausplots_vouchers_richness.R
+++ compile_ausplots_vouchers_richness.R
@@ -1,5 +1,6 @@
-#Vouchered records and species richness
-#This script compiles the vouchered species records from each plot, as a separate exercise to compiling species records from the point intercept hits. As each species observed in the plot is vouchered, this represents a more complete list of species, although abundances only from PI datasets. 
+#Vouchered species records.
+
+#This script compiles the vouchered species records from each AusPlots site. This is a separate exercise to compiling species records from the point intercept hits. As each species observed in the plot is vouchered, this represents a more complete list of species, although abundances are only available from PI the dataset. 
 #
 #Authors:
 #Greg Guerin
@@ -9,15 +10,15 @@
 library(R.utils) #for capitalisation of names
 library(plyr) #for count function
 
-file_list_vouch <- Sys.glob("~/TERN_analysis/extractedData/veg/vegVouchers*.csv") #list of files with same prefix, in this case the vegVouchers files (list of vouchered species records from plots)
+file_list_vouch <- Sys.glob("./extractedData/veg/vegVouchers*.csv") #list of files with same prefix, in this case the vegVouchers files (list of vouchered species records from plots)
 
 #function to open files off a list, and append the data to a single output file:
 extract_vouch <- function(file_list) {
-	working_file <- read.csv(file=file_list[1], sep=",", header=TRUE)	
+	working_file <- read.csv(file=file_list[1])	
 	document <- as.data.frame(matrix(nrow=1, ncol=ncol(working_file))) 
 	colnames(document) <- colnames(working_file)
 	for (i in 1:length(file_list)) {
-	working_file <- try(read.csv(file=file_list[i], sep=",", header=TRUE))
+	working_file <- try(read.csv(file=file_list[i]))
 	if(class(working_file)=="try-error") {
 		working_file <- NULL
 	}
@@ -29,21 +30,15 @@ extract_vouch <- function(file_list) {
 	}
 document <- document[-1,]
 write.csv(document, file="vegVouchers_compiled.txt")
-}
+} #close function
 
-extract_vouch(file_list_vouch)
+extract_vouch(file_list_vouch) #run the function on the file list
 
-vouch <- read.table(file="vegVouchers_compiled.txt", sep=",", header=TRUE) #read this file back into R
+vouch <- read.csv(file="vegVouchers_compiled.txt") #read this file back into R
 
 #some cleaning operations on the names:
 vouch$herbarium_determination <- trim.trailing(vouch$herbarium_determination)
 vouch$herbarium_determination <- tolower(vouch$herbarium_determination)
 vouch$herbarium_determination <- capitalize(vouch$herbarium_determination)
 
-#calculate species richness per plot
-
-richness <- count(vouch, c("site_location_name", "herbarium_determination")) #counts number of rows with same site and species name, and therefore removes duplicates
-richness$freq <- 1 #to avoid weighting (each row adds one to count below)
-richness <- count(richness, "site_location_name") #now count how many rows (spp) per plot - 'richness' is now a df of plots with their species richness
-
-write.csv(richness, file="spp_rich.txt")
+vouch$site_unique <- do.call(paste, c(vouch[c("site_location_name", "site_location_visit_id")], sep = "-"))
