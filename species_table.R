eda81893dcbfe293fb14c5108cac98be0ff13ccd
Greg Guerin <gregguerin@WC02H306WDHJV.ad.adelaide.edu.au>
2018-08-08 17:50:42 +0930

    New self-contained function for occurrence tables

---
+++ species_table.R
@@ -0,0 +1,107 @@
+
+species_table <- function(veg.PI, m_kind=c("PA", "percent_cover", "freq", "IVI"), cover_type=c("PFC", "OCC")) {
+
+#this is a self-contained function that takes a data frame of individual raw point intercept hits from ausplots and generates species occurrence matrices as desired based on presence, cover, frequencey or IVI index. The output is a species v sites table as a data frame.
+
+#veg.PI is the input point intercept data (raw)
+
+#m_kind is the desired species scoring method: binary (presence/absence), percent cover, frequency (based on occurrences on transects within plots) or IVI (combination of cover and frequency)
+
+#cover_type is a choice between 'projected foliage cover' and 'opaque canopy cover' and only applies to percent_cover and IVI. If 'PFC' is selected, hits scored as 'in canopy sky' are removed, whereas they are retained as cover for that species for 'OCC'
+
+require(simba)
+require(plyr)
+
+hits <- veg.PI
+
+
+if(m_kind == "PA") {
+	
+	hits <- hits[which(!duplicated(hits[,c("site_unique", "herbarium_determination"),])), c("site_unique", "herbarium_determination")] #remove duplicated hits (i.e. same species in a given plot - we just want binary presence/absence here)
+	
+	hits <- na.omit(hits) #remove hots not determined as a species
+	
+	hits$presence <- rep(1, nrow(hits)) #add a column of '1's for presence (for mama function)
+	
+	cover_matrix_binary <- mama(hits) #generate an occurrence matrix (binary)
+	
+	species_matrix <- cover_matrix_binary
+} # end PA section
+
+
+
+if(m_kind=="percent_cover" | m_kind == "IVI") {
+	
+
+#count combined (PI) cover scores for each species in each plot:
+total.points.fun <- function(x) {return(length(unique(hits[which(hits$site_unique == x),]$hits_unique)))} #function to go through a list of plot names and count how many unique hits there were (for a standard plot, this will equal 1010)
+
+
+total.points <- data.frame(site_unique = unique(hits$site_unique), total.points = unlist(lapply(unique(hits$site_unique), total.points.fun))) #site/visit and associated number of unique PI hits taken, by applying the above function
+
+#remove in canopy sky hits if projected foliage cover required:
+if(cover_type == "PFC") {
+	hits <- hits[which(as.character(hits$in_canopy_sky) == "FALSE"),]
+} #close if PFC
+
+
+covers <- count(hits, c("site_unique", "herbarium_determination")) #counts number of rows with same site and species name
+
+covers <- merge(covers, total.points, by="site_unique") #merge to get the hit counts for species in a df along with the total hits taken for each plot
+
+covers$percent <- covers$freq/covers$total.points*100 #add a new column to convert from number of hits to percent. Some plots have less or more than the standard 1010 PI hits for various reasons, so this ensures dividing by the actual number of unique hits, which is calculated form the raw data and unique transect/number combos
+
+covers <- na.omit(covers) #to remove percents for records with no herbarium determination
+
+
+#generate sites by species matrix:
+cover_matrix <- mama(covers[,-c(3, 4)]) #third/fourth columns (count of individual hits and total hits) excluded as we want to the mama function to read our fourth column 'percent' [percent cover] as the abundance value (assumed to be the 3rd column): this generates a data.frame with sites as rows and species as columns, with percent covers from PI hits as values
+
+species_matrix <- cover_matrix
+} # close cover matrix section
+
+
+
+if(m_kind=="freq" | m_kind == "IVI") {
+	
+	
+	hits <- na.omit(hits) #remove NA herbarium determinations
+	
+	transects <- count(hits, c("site_unique", "herbarium_determination", "transect")) #count PI records for each uniqe plot/species/transect combo
+	
+	transects$freq <- 1 #revert to 1 rather than a count (presence on a transect within a plot)
+	
+	freqs <- count(transects, c("site_unique", "herbarium_determination")) #count transect presences per species per plot, e.g. 5 means that species was recorded on 5 of the transects
+	
+	number.transects <- count(transects[!duplicated(transects[,c("site_unique", "transect")]),], "site_unique") #some plots have more or less than 10 transects so this is a data frame of site_unique IDs and the number of transects
+	
+	freqs <- merge(freqs, number.transects, by="site_unique") #this maps the number of recorded transects for each plot to the species records and number of transect hits for each. Columns are now freq.x and freq.y
+	
+	freqs$freq.z <- freqs$freq.x/freqs$freq.y #divide by the actual number of transects (should range from 0.1 to 1)
+	
+	freq_matrix <- mama(freqs[,c(1,2,5),]) #create a species ~ sites matrix with the frequencies as values, and zeros if species occur on no transect for a plot; selecting columns 1,2 and 5 to exclude the freq.x and freq.y cols for mam
+	
+	species_matrix <- freq_matrix
+
+
+} #end M-kind == freq
+
+
+
+if(m_kind == "IVI") {
+	freq_matrix_percents <- freq_matrix*100 #convert to %
+		
+	IVI <- (cover_matrix + freq_matrix_percents)/2  #combine freq and cover to get IVI
+	
+	species_table <-  IVI #note that the frequencies appear to dominate the output as they are overall much higher than the covers
+
+} #end IVI section
+
+
+
+
+
+return(species_matrix)
+	
+} #end function
+
