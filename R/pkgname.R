c97a8d413346378f78942ebe2b5e50fcfefa3570
Greg Guerin <gregguerin@WC02H306WDHJV.ad.adelaide.edu.au>
2018-08-24 17:33:27 +0930

    At package info file

---
+++ R/pkgname.R
@@ -0,0 +1,29 @@
+#' Overview of ausplotsR
+#'
+#' \code{ausplotsR} explain what it is for.
+#' The package is under development and additional functions will be added over time.
+#'
+#' @details The package includes these functions:
+#' 
+#' \code{\link{get_ausplots}}: This function is the starting point for accessing data through the ausplotsR package for the following survey modules:
+#' \itemize{
+#'   \item Site information
+#'   \item Vegetation point intercept
+#'   \item Vegetation vouchers
+#'   \item Basal wedge
+#'   \item Vegetation structural summaries
+#'   \item Soils and soil samples information
+#'   ...
+#' }
+#' 
+#' \code{\link{species_table}}: Species occurrence matrices for AusPlots.
+#' 
+#' \code{\link{growth_form_table}}: Plant growth form occurrence matrices for TERN AusPlots.
+#' 
+#' \code{\link{fractional_cover}}: Fractional cover based on TERN AusPlots data.
+#' 
+#' \code{\link{basal_area}}: Basal area of tree trunks in TERN AusPlots.
+#' 
+#' @seealso \code{\link{animate_move}}
+#' @author Greg Guerin, Tom Saleeba, Andrew Tokmakoff. Maintainer: Greg Guerin ggueri01@gmail.com
+"_PACKAGE"
