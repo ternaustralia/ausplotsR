4774f937e7a9a276640d0e19f8a978e6703a6f2f
Tom Saleeba <tom.saleeba@gmail.com>
2019-01-29 18:10:33 +1030

    feat: AP-13 add bounding box support to get_samples() function

--- R/get_samples.R
+++ R/get_samples.R
@@ -1,6 +1,19 @@
-get_samples <- function(my.Plot_IDs=c(), species_name_search=NULL) {
+.is_2_element_double_vector <- function(vec) {
+  return(is.vector(vec) && is.double(vec) && length(vec) == 2)
+}
 
-  result <- extract_samples(Plot_IDs=my.Plot_IDs, species_name_search=species_name_search)
+get_samples <- function(my.Plot_IDs=c(), species_name_search=NULL, lat_boundary=c(-90, 90), lon_boundary=c(-180, 180)) {
+
+  is_not_null_or_expected_typed_vector <- ( !is.null(my.Plot_IDs) && !is.vector(my.Plot_IDs) ) || ( length(my.Plot_IDs) > 0 && !is.character(my.Plot_IDs) )
+  if(is_not_null_or_expected_typed_vector) stop("my.Plot_IDs must be a vector of strings/characters")
+
+  if(!is.null(species_name_search) && (!is.character(species_name_search) || is.vector(species_name_search))) stop("species_name_search must be a single string/characters (not a vector)")
+
+  if(!.is_2_element_double_vector(lat_boundary)) stop("lat_boundary must be a vector with 2 double elements. First element = min, second = max")
+
+  if(!.is_2_element_double_vector(lon_boundary)) stop("lon_boundary must be a vector with 2 double elements. First element = min, second = max")
+
+  result <- extract_samples(Plot_IDs=my.Plot_IDs, species_name_search=species_name_search, lat_boundary, lon_boundary)
 
   result$site_unique <- do.call(paste, c(result[c("site_location_name", "site_location_visit_id")], sep = "-")) #add unique site/visit identifier
 
