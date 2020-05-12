b046e82b61f45ba407943c1beb7b6b56aa75ce6e
Greg Guerin <GregG@192-168-1-14.tpgi.com.au>
2020-05-12 11:33:11 +0930

    Add default data extract

--- R/ausplots_visual.R
+++ R/ausplots_visual.R
@@ -1,8 +1,14 @@
-ausplots_visual <- function(my.ausplots.object, map = TRUE, fractional.pie = TRUE, growthform.pie = TRUE, cumulative_cover = TRUE, whittaker = TRUE, outfile=NULL) {
+ausplots_visual <- function(my.ausplots.object = NULL, map = TRUE, map.attribute = TRUE, fraction.pie = TRUE, growthform.pie = TRUE, cumulative_cover = TRUE, whittaker = TRUE, outfile=NULL) {
 	
 	#check input formats
-
-
+	
+	
+	
+	
+	#check data
+	if(!missing(my.ausplots.object)) {
+		my.ausplots.object <- get_ausplots(bounding_box=c(130, 140, -25, -15), veg.vouchers=FALSE)
+	}
 
 	#start plot
 	if(missing(outfile)) {
@@ -11,7 +17,9 @@ ausplots_visual <- function(my.ausplots.object, map = TRUE, fractional.pie = TRU
 	pdf(outfile)
 	
 	#call individual graphical functions that are switched on
-	if(fractional.pie) {}
+	if(map) {} #
+	if(map.attribute) {}
+	if(fraction.pie) {}
 	if(growthform.pie) {}
 	if(cumulative_cover) {}
 	if(whittaker) {}
