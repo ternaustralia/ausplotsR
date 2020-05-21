a5c19dbcd6cc5d56b0cb45dd85fe8e7a82603342
Greg Guerin <GregG@192-168-1-14.tpgi.com.au>
2020-05-21 10:45:04 +0930

    Legend fail check
    Make 'try' for legend as it failed for a random plot (no growth
    forms???) and crashed the pdf output

--- R/growthform_pie.R
+++ R/growthform_pie.R
@@ -3,6 +3,6 @@ growthform_pie <- function(GF, colours) {
 	GF <- GF[,-(which(colSums(GF) == 0))]
 	
 	pie(x=as.numeric(GF), labels=round(as.numeric(GF), digits=0), main=row.names(GF), col=colours[names(GF)])
-	legend("right", legend=names(GF), pch=20, col=colours[names(GF)], bty="n", cex=0.7, pt.cex=1)
+	try(legend("right", legend=names(GF), pch=20, col=colours[names(GF)], bty="n", cex=0.7, pt.cex=1))
 	
 }
