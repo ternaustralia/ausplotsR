7f4fc11eebe8f562e2128bf4d28cb8b12f1d6908
Greg Guerin <GregGuerin@users.noreply.github.com>
2020-06-02 12:03:06 +0930

    Restructure function
    Structural rearrangement, addition of combined plotting function, more efficient processing etc

--- R/species_optimisation.R
+++ R/species_optimisation.R
@@ -1,22 +1,66 @@
-optim _species <- function (speciesVsitesMatrix, n.plt=250, start=”fixed”, plot=FALSE, richness=TRUE, RRR=TRUE, CWE=TRUE, shannon=TRUE, simpson=TRUE, simpson_beta=TRUE){
-  if(richness){
-    speciesVsitesMatrix_binary <- speciesVsitesMatrix
-    speciesVsitesMatrix_binary[speciesVsitesMatrix_binary > 0] <- 1 #convert abundances to presences
-  } #end binary conversion
-  if(RRR){
-    speciesVsitesMatrix_binary <- speciesVsitesMatrix
-    speciesVsitesMatrix_binary[speciesVsitesMatrix_binary > 0] <- 1 #convert abundances to presences
-  } #end binary conversion
-  if(CWE){
-    speciesVsitesMatrix_binary <- speciesVsitesMatrix
-    speciesVsitesMatrix_binary[speciesVsitesMatrix_binary > 0] <- 1 #convert abundances to presences
-  } #end binary conversion
-  if(simpson_beta){
-    speciesVsitesMatrix_binary <- speciesVsitesMatrix
-    speciesVsitesMatrix_binary[speciesVsitesMatrix_binary > 0] <- 1 #convert abundances to presences
-  } #end binary conversion
+optim_species <- function(speciesVsitesMatrix, n.plt=250, start="fixed", plot=TRUE, richness=TRUE, RRR=TRUE, CWE=TRUE, shannon=TRUE, simpson=TRUE, simpson_beta=TRUE, frequent=TRUE, random=TRUE, iterations=10) {
+	
+############################
+#check inputs
+	
+	if(n.plt > nrow(speciesVsitesMatrix)) {
+		cat("You are attempting to optimise more sites that exist in the dataset - trimming to maximum. \n")
+		n.plt <- nrow(speciesVsitesMatrix)
+		}
+		
+	if(any(c(richness, RRR, CWE, simpson_beta, frequent))) {
+		 speciesVsitesMatrix_binary <- speciesVsitesMatrix
+		 speciesVsitesMatrix_binary[speciesVsitesMatrix_binary > 0] <- 1 #convert abundances to presences
+	}
+	
+########################
+#calls
+
+	result <- list()
+	
+	if(richness){
+    result$Richness <- Richness.opt(speciesVsitesMatrix_binary, n.plt) #
+  } #end if richness
   
-}
+  if(RRR){
+    result$RRR <- RRR.opt(speciesVsitesMatrix_binary, n.plt) #
+  } #end if RRR
+  
+  if(CWE){
+    result$CWE <- CWE.opt(speciesVsitesMatrix_binary, n.plt) #
+  } #end if CWE
+  
+  if(simpson_beta){
+    result$SimpsonBeta <- simpson_beta.opt(speciesVsitesMatrix_binary, n.plt, start=start) #
+  } #end if simpson beta
+  
+  if(shannon) {
+  	result$Shannon <- Shannon.opt(speciesVsitesMatrix, n.plt) #
+  } #end if shannon
+  
+  if(simpson) {
+  	result$Simpson <- Simpson.opt(speciesVsitesMatrix, n.plt) #
+  } #end if simpson
+   
+  if(frequent) {
+  	result$Frequent <- Frequent_simpson_beta.opt(speciesVsitesMatrix_binary, n.plt, iterations) 
+  } #end if freqent plots
+  
+   if(random) {
+  	result$Random <- Random.opt(speciesVsitesMatrix, n.plt, iterations)
+  } #end if random
+  
+  
+  ##########################
+  #wrap up:
+  
+  if(plot) {
+  	plot.opt(result)
+  	} #end if plot
+    
+  return(result)
+  
+} #end function
 
 
 ########################
@@ -25,11 +69,9 @@ Richness.opt <- function(speciesVsitesMatrix_binary, n.plt) {
   RichnessSort <- rev(sort(Richness))[1:n.plt] #inverse of the sort function to get decreasing order
   RichnessMCP <- RichnessSort[1:n.plt] #get top n.plt plots based on Richness
   RichnessMCPaccum <- specaccum(speciesVsitesMatrix_binary[names(RichnessMCP),], method="collector") #get a species accumulation curve for these selected plots
-  plot (RichnessMCPaccum, col="magenta", lwd=2,  main="Richness optimiser", xlab = "Number of plots", ylab = "Cumulative species")
-  return(RichnessMCPaccum)
+   return(RichnessMCPaccum)
 }  
 
-Richness.opt(speciesVsitesMatrix_binary) #check
 
 ########################
 RRR.opt <- function(speciesVsitesMatrix_binary, n.plt) {
@@ -37,23 +79,20 @@ RRR.opt <- function(speciesVsitesMatrix_binary, n.plt) {
   RRRSort <- rev(sort(RRR)) #inverse of the sort function to get decreasing order
   RRRMCP <- RRRSort[1:n.plt] #get top n.plt plots based on RRR
   RRRMCPaccum <- specaccum(speciesVsitesMatrix_binary[names(RRRMCP),], method="collector") #get a species accumulation curve for these selected plots
-  plot(RRRMCPaccum, col="cyan", lwd=2, main="RRR optimiser", xlab = "Number of plots", ylab = "Cumulative species")
   return(RRRMCPaccum)
 }
 
-RRR.opt(speciesVsitesMatrix_binary, n.plt) #check
 
 ########################
 CWE.opt <- function(speciesVsitesMatrix_binary, n.plt) {
   CWE <- rowSums(speciesVsitesMatrix_binary/colSums(speciesVsitesMatrix_binary))/rowSums(speciesVsitesMatrix_binary) #It's RRR divided by richness
   CWESort <- rev(sort(CWE)) #inverse of the sort function to get decreasing order
   CWEMCP <- CWESort[1:n.plt] #get top n.plt plots based on CWE
-  CWEMCPaccum <- specaccum(PAdataGood[names(CWEMCP),], method="collector") #get a species accumulation curve for these selected plots
-  plot(CWEMCPaccum, col="gold", lwd=2,main="CWE optimiser", xlab = "Number of plots", ylab = "Cumulative species")
+  CWEMCPaccum <- specaccum(speciesVsitesMatrix_binary[names(CWEMCP),], method="collector") #get a species accumulation curve for these selected plots
+  #plot(CWEMCPaccum, col="gold", lwd=2,main="CWE optimiser", xlab = "Number of plots", ylab = "Cumulative species")
   return(CWEMCPaccum)
 }
 
-CWE.opt(speciesVsitesMatrix_binary, n.plt) #check
 
 ########################
 Shannon.opt <- function(speciesVsitesMatrix, n.plt) {
@@ -61,11 +100,9 @@ Shannon.opt <- function(speciesVsitesMatrix, n.plt) {
   ShannonSort <- rev(sort(Shannon)) #inverse of the sort function to get decreasing order
   ShannonMCP <- ShannonSort[1:n.plt] #get top n.plt plots based on Shannon-Wienner diversity index
   ShannonMCPaccum <- specaccum(speciesVsitesMatrix[names(ShannonMCP),], method="collector") #get a species accumulation curve for these selected plots
-  plot(ShannonMCPaccum, col="mediumseagreen", lwd=2, main="Sannon-Wienner optimiser", xlab = "Number of plots", ylab = "Cumulative species")
   return(ShannonMCPaccum)
 }
 
-Shannon.opt(speciesVsitesMatrix) #check
 
 ########################
 Simpson.opt <- function(speciesVsitesMatrix, n.plt) {
@@ -73,75 +110,95 @@ Simpson.opt <- function(speciesVsitesMatrix, n.plt) {
   SimpsonSort <- rev(sort(Simpson)) #inverse of the sort function to get decreasing order
   SimpsonMCP <- SimpsonSort[1:n.plt] #get top n.plt plots based on Shannon-Wienner diversity index
   SimpsonMCPaccum <- specaccum(speciesVsitesMatrix[names(SimpsonMCP),], method="collector") #get a species accumulation curve for these selected plots
-  plot(SimpsonMCPaccum, col="purple", lwd=2, main="Gini-Simpson optimiser", xlab = "Number of plots", ylab = "Cumulative species")
   return(SimpsonMCPaccum)
 }
 
-Simpson.opt(speciesVsitesMatrix, n.plt) #check
 
 ########################
-
 simpson_beta.opt <- function(speciesVsitesMatrix_binary, n.plt, start) {
-  richest.plot<- which.max(rowSums(speciesVsitesMatrix_binary))
-  if (start =="fixed"){
-    start.plot<- speciesVsitesMatrix_binary[richest.plot,] #fixed seed: this is the richest plot
+	 original_matrix <- speciesVsitesMatrix_binary
+  if (start == "fixed"){
+  	start.plot <- rownames(speciesVsitesMatrix_binary)[which.max(rowSums(speciesVsitesMatrix_binary))] #fixed seed: this is the richest plot
+  } #end if fixed
+  if (start == "random"){
+    start.plot <- sample(rownames(speciesVsitesMatrix_binary), 1) #get a random seed plot
   }
-  if (start=="random"){
-    start.plot <- speciesVsitesMatrix_binary[sample(nrow(speciesVsitesMatrix_binary), 1), ] #get a random seed plot
-  }
-    result <- list() 
+  result <- list() 
   n <- 1
-  result[n] <- row.names(start.plot)
+  result[n] <- start.plot
   for(i in 1:(n.plt-1)) {
     n <- n + 1 
     simpson <- as.data.frame(as.matrix(beta.pair(speciesVsitesMatrix_binary)$beta.sim)) #simpson beta diversity between all pairs (excludes species nestedness)
-    sort.diss <- rev(sort(simpson[row.names(simpson) %in% row.names(start.plot),])) # creates vector of dissimilarity comparisons
-    next.plot.name <- names(sort.diss[1]) #selected the first plot in the vector, which after sorting is the most dissimilar
+    sort.diss <- rev(sort(simpson[start.plot,])) #creates single row data frame holding the vector of dissimilarity comparisons to the seed/start.plot
+    next.plot.name <- names(sort.diss)[1] #select the first plot in the vector, which after sorting is the most dissimilar
+    if(next.plot.name == start.plot) {next.plot.name <- names(sort.diss)[2]}
     result[n] <- next.plot.name #add it to the list of plots to save
-    cat(next.plot.name, " ", sort.diss[[1]], "\n") #Outputs the objects, concatenating the representations in a chain or series
-    start.plot <- start.plot + speciesVsitesMatrix_binary[next.plot.name,] #Merge the seed plot with the first selected plot and get all occurrences into one virtual plot
-    start.plot[start.plot>0] <- 1 #set all values to 1 so it is PA data
-    speciesVsitesMatrix_binary[row.names(speciesVsitesMatrix_binary) %in% row.names(start.plot),] <- start.plot 
+    cat(next.plot.name, " ", sort.diss[,1], "\n") #Print out the chosen plot and its dissimilarity score
+    speciesVsitesMatrix_binary[start.plot,] <- speciesVsitesMatrix_binary[start.plot,] + speciesVsitesMatrix_binary[next.plot.name,] #Merge the seed plot with the latest selected plot to get all occurrences into one virtual plot
+    speciesVsitesMatrix_binary[start.plot,][speciesVsitesMatrix_binary[start.plot,] > 0] <- 1 #set all values to 1 so it is PA data in case species are shared
+    speciesVsitesMatrix_binary <- speciesVsitesMatrix_binary[(!rownames(speciesVsitesMatrix_binary) %in% next.plot.name),]
   }
-  dissimilarplots <- unlist(result)
-  return(dissimilarplots)
+  dissimilarplots <- unlist(result) #vector of plot names in order selected
+  return(specaccum(original_matrix[dissimilarplots,], method="collector"))
 }
 
-simpson_beta.opt(speciesVsitesMatrix_binary, n.plt) #check
 
 ########################
-
-Frequent_simpson_beta.opt<- simpson_beta.opt(speciesVsitesMatrix_binary, n.plt, start = "random", iterations = 1000, freq = TRUE){
-   if (iterations == TRUE){
-    opt.runs <- list()
+Frequent_simpson_beta.opt <- function(speciesVsitesMatrix_binary, n.plt, iterations) {
+    opt.runs.freq <- list()
     n <- 0
-    for(i in 1:iterations){
+    for(i in 1:iterations) {
       n <- n + 1
-      opt.run <- simpson_beta.opt(speciesVsitesMatrix_binary, n.plt, start = "random")
-      opt.runs.freq[[n]]<-opt.run
       cat("Rep ", n, "\n")
-      Sppaccum_freq[[n]]<- specaccum(speciesVsitesMatrix_binary[sample(nrow(speciesVsitesMatrix_binary), n.plt),], method="collector")
-      }  
-    combined.rand_specaccum <- Sppaccum_freq[[1]] #copy one for format
-    combined_matrix_rand <- do.call(rbind, lapply(Sppaccum_freq, function(x) {return(x$richness)})) #compile the  cumulative richness results from reps above into a matrix
-    combined.rand_specaccum$richness <- apply(combined_matrix_rand, 2, mean) #using the matrix, calculate the mean for each additional plot, and add that to the richness slot inthe specaccum object
-    combined.rand_specaccum$sd <- apply(combined_matrix_rand, 2, sd) #same for standard deviation
-    combined.rand_specaccum$method <- "random" #assign it as random not collector so it plots correctly
-    plot(combined.rand_specaccum, bty="l", las=1, col="beige", lwd=2, main="Random seed for Simpson dissimilarity optimisation", xlab = "Number of plots", ylab = "Cumulative species")
-    return(combined.rand_specaccum) #mean line with bars for sd
-   
-    if (freq == TRUE){
-    freq_list <- unlist(opt.runs.freq)
-    freq_table = table(freq_list)
-    freq_dataframe = as.data.frame(freq_table)
-    sorted_freq_plots<-freq_dataframe[order(freq_dataframe$Freq, decreasing=T),]
-    top_freq_plots<-sorted_freq_plots %>% dplyr::slice(1:n.plt) # most frequent plots
-    top_freq_plots_PAdata<-speciesVsitesMatrix_binary[top_freq_plots$freq_list,] #subset the most frequent plots 
-    SPPaccum_freq<- specaccum(top_freq_plots_PAdata, method="collector") 
-    plot(SPPaccum_freq, lwd=2, add= T, col="blue")
-    return(top_freq_plots)
-    }
+      opt.runs.freq[[n]] <- simpson_beta.opt(speciesVsitesMatrix_binary, n.plt, start = "random")
+      } #end iterations
+    freq_plots <- plyr::count(unlist(lapply(opt.runs.freq, FUN=function(x) as.character(names(x$richness)))))
+    freq_plots <- freq_plots[rev(order(freq_plots$freq)),]
+    freq_plots <- freq_plots[1:n.plt,] 
+    freq_accum <- specaccum(speciesVsitesMatrix_binary[as.character(freq_plots$x),], method="collector")
+    return(freq_accum)
 }  
   
-Frequent_simpson_beta.opt(speciesVsitesMatrix, n.plt, start = "random", iterations = 1000, freq = TRUE) #check  
+  
+####################################
+Random.opt <- function(speciesVsitesMatrix, n.plt, iterations) {
+    Sppaccum_freq <- list() #
+    n <- 0
+    for(i in 1:iterations) {
+      n <- n + 1
+      cat("Rep ", n, "\n")
+      Sppaccum_freq[[n]]<- specaccum(speciesVsitesMatrix[sample(nrow(speciesVsitesMatrix), n.plt),], method="collector")
+      }  
+    combined.rand_specaccum <- Sppaccum_freq[[1]] #copy one for format
+    combined_matrix_rand <- do.call(rbind, lapply(Sppaccum_freq, function(x) {return(x$richness)})) #compile the cumulative richness results from reps above into a matrix
+    combined.rand_specaccum$richness <- apply(combined_matrix_rand, 2, mean) #using the matrix, calculate the mean for each additional plot, and add that to the richness slot in the specaccum object
+    combined.rand_specaccum$sd <- apply(combined_matrix_rand, 2, sd) #same for standard deviation
+    combined.rand_specaccum$method <- "random" #assign it as random not collector so it plots correctly as mean and SD
+    return(combined.rand_specaccum) #object that can be plotted with mean line with bars for sd
+} 
+
+
+#################################
+plot.opt <- function(result, choices=c("Richness", "RRR", "CWE", "Shannon", "Simpson", "SimpsonBeta", "Frequent", "Random")) {
+  	result <- result[names(result) %in% choices]
+  	plot(1, ylim=c(0, max(unlist(lapply(result, FUN=function(x) max(x$richness))))), xlim=c(0, length(result[[1]]$richness)), type="n", xlab = "Number of plots", ylab = "Cumulative species", main="Optimisation etc...", las=1, bty="l", cex.main=1.2) #blank template plot
+    opt.col <- sample(rainbow(7), length(which(names(result) %in% choices[1:7]))) #random colours for the plot lines of optimisers, but exclude random for now
+    zzz <- 0
+    for(j in result) { #for each optimiser result in the list
+      zzz <- zzz + 1
+      if(names(result)[zzz] != "Random") {
+      	plot(result[[zzz]], col=opt.col[zzz], lwd=1.5, add=TRUE) #add line for each optimiser
+      	} #end if not random
+      if(names(result)[zzz] == "Random") {
+      	plot(result[[zzz]], col="gray", lwd=1, lty=2, add=TRUE) #add line for random
+      } #end if Random
+    } #close for j in ...
+    if("Random" %in% names(result)) {
+    	legend("bottomright", legend=names(result), lty=c(rep(1, (length(result)-1)), 2), col=c(opt.col[1:(length(result)-1)], "gray"), cex=1, bty='n', lwd=rep(3, length(result))) 	
+    } #end if Random
+    if(!"Random" %in% names(result)) {
+    	legend("bottomright", legend=names(result), lty=rep(1, length(result)), col=opt.col[1:length(result)], cex=1, bty='n', lwd=rep(3, length(result))) 	
+    } #end if not Random
+  } #end plot.opt function
+
  
