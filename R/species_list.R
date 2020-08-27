addc099ea673aaf1f307b07a5f0914c997b8a348
Greg Guerin <GregGuerin@users.noreply.github.com>
2020-08-27 17:30:30 +0930

    Update species_list.R
    Clean up and activate/uncomment strip_bryophytes action

--- R/species_list.R
+++ R/species_list.R
@@ -1,8 +1,8 @@
 species_list <- function(veg.vouch, grouping=c("by_site", "by_visit", "collapse"), species_name=c("SN","HD","GS"), strip_bryophytes=FALSE, append_family=FALSE, writefile=FALSE, outfile="my_species_lists.txt") {
   
+  #input checks and set defaults
   if(!class(veg.vouch) == "data.frame") {stop("veg.PI must be a data.frame")}
   if(any(!c("site_location_name", "herbarium_determination") %in% names(veg.vouch))) {stop("Can't match names of veg.vouch; data frame should be returned from get_ausplots")}
-  
   
   if(any(!is.logical(c(strip_bryophytes, append_family, writefile)))) {
     stop("Arguments 4:6 must be TRUE or FALSE")
@@ -18,7 +18,6 @@ species_list <- function(veg.vouch, grouping=c("by_site", "by_visit", "collapse"
     warning("No grouping supplied, defaulting to 'by_site'")
   } #end missing
   
-  
   if(!grouping %in% c("by_site", "by_visit", "collapse")){
     stop("Grouping argument does not match available choices")
   }
@@ -28,7 +27,8 @@ species_list <- function(veg.vouch, grouping=c("by_site", "by_visit", "collapse"
   }
   
     
-  #if(strip_bryophytes) {veg.vouch <- subset(veg.vouch, group !="bryophyte")}
+  if(strip_bryophytes) {veg.vouch <- subset(veg.vouch, group != "bryophyte")}
+  
   
   if(species_name == "SN") {
     veg.vouch <- subset(veg.vouch, !is.na(standardised_name))
@@ -101,14 +101,5 @@ species_list <- function(veg.vouch, grouping=c("by_site", "by_visit", "collapse"
     write.table(do.call(rbind, lapply(final_list, function(x) paste(x,collapse=", "))), file=outfile)
   }
   
- 
   return(final_list)
-  #return(do.call(rbind, lapply(final_list, function(x) paste(x,collapse=", "))))
-  #e.g., :: lapply(final_list, function(x) paste(x, collapse=", "))
-
-
-      
-#call
-  #species_list(vvv$veg.vouch, species_name="GS")
-       
 }
