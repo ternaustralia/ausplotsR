ecff289704f9f4b2712569b4ab8cf8a863db7774
Greg Guerin <GregGuerin@users.noreply.github.com>
2020-10-28 13:41:33 +1030

    Update species_optimisation.R
    Exchange message for cat so text on console can be more easily suppressed (CRAN requirement); add verbose=TRUE argument to main function  and pass to random, frequent and simpson sub-functions with if(verbose) added whenever iteration prgress is printed to the console with cat.

--- R/species_optimisation.R
+++ R/species_optimisation.R
@@ -1,10 +1,10 @@
-optim_species <- function(speciesVsitesMatrix, n.plt=250, richness=TRUE, RRR=TRUE, CWE=TRUE, shannon=TRUE, simpson=TRUE, simpson_beta=TRUE, start="fixed", plot_name=NULL, frequent=TRUE, random=FALSE, iterations=10, plot=TRUE) {
+optim_species <- function(speciesVsitesMatrix, n.plt=250, richness=TRUE, RRR=TRUE, CWE=TRUE, shannon=TRUE, simpson=TRUE, simpson_beta=TRUE, start="fixed", plot_name=NULL, frequent=TRUE, random=FALSE, iterations=10, plot=TRUE, verbose=TRUE) {
 	
 ############################
 #check inputs
   
   if(!is.numeric(speciesVsitesMatrix[,1])) {
-    cat("Non-numeric first column... assuming these are site names. \n")
+    message("Non-numeric first column... assuming these are site names. \n")
     rownames(speciesVsitesMatrix) <- speciesVsitesMatrix[,1]
     speciesVsitesMatrix <- speciesVsitesMatrix[,-1] #remove the first column
   }#if the first column of the dataset corresponds to the site names, then apply site names to the rows and remove the column
@@ -14,7 +14,7 @@ optim_species <- function(speciesVsitesMatrix, n.plt=250, richness=TRUE, RRR=TRU
   if(!is.null(plot_name) && (!plot_name %in% rownames(speciesVsitesMatrix))) {stop("Selected plot_name must match a site/row name in the species~sites data.")}
   
 	if(n.plt > nrow(speciesVsitesMatrix)) {
-		cat("You are attempting to optimise more sites that exist in the dataset - trimming to maximum. \n")
+		message("You are attempting to optimise more sites that exist in the dataset - trimming to maximum. \n")
 		n.plt <- nrow(speciesVsitesMatrix)
 	}
   
@@ -41,7 +41,7 @@ optim_species <- function(speciesVsitesMatrix, n.plt=250, richness=TRUE, RRR=TRU
   } #end if CWE
   
   if(simpson_beta){
-    result$SimpsonBeta <- simpson_beta.opt(speciesVsitesMatrix_binary, n.plt, start=start, plot_name) #
+    result$SimpsonBeta <- simpson_beta.opt(speciesVsitesMatrix_binary, n.plt, start=start, plot_name, verbose) #
   } #end if simpson beta
   
   if(shannon) {
@@ -53,13 +53,13 @@ optim_species <- function(speciesVsitesMatrix, n.plt=250, richness=TRUE, RRR=TRU
   } #end if simpson
    
   if(frequent) {
-  	hold <- Frequent_simpson_beta.opt(speciesVsitesMatrix_binary, n.plt, iterations) 
+  	hold <- Frequent_simpson_beta.opt(speciesVsitesMatrix_binary, n.plt, iterations, verbose) 
     result$Frequent <- hold$Freq
     result$SimpsonBeta_randSeed <- hold$simspon_rand
   } #end if freqent plots
   
    if(random) {
-  	result$Random <- Random.opt(speciesVsitesMatrix_binary, n.plt, iterations)
+  	result$Random <- Random.opt(speciesVsitesMatrix_binary, n.plt, iterations, verbose)
   } #end if random
   
   
@@ -126,7 +126,7 @@ Simpson.opt <- function(speciesVsitesMatrix, n.plt) {
 
 
 ########################
-simpson_beta.opt <- function(speciesVsitesMatrix_binary, n.plt, start, plot_name) { #I added plot_name in case the user wants to define a fixed seed which does not correspond to the richest site
+simpson_beta.opt <- function(speciesVsitesMatrix_binary, n.plt, start, plot_name, verbose) { #I added plot_name in case the user wants to define a fixed seed which does not correspond to the richest site
 	 original_matrix <- speciesVsitesMatrix_binary
   if (start == "fixed"){
   	start.plot <- rownames(speciesVsitesMatrix_binary)[which.max(rowSums(speciesVsitesMatrix_binary))] #fixed seed: this is the richest plot
@@ -153,7 +153,7 @@ simpson_beta.opt <- function(speciesVsitesMatrix_binary, n.plt, start, plot_name
     }
     #if(next.plot.name == start.plot) {next.plot.name <- names(sort.diss)[2]}
     result[n] <- next.plot.name #add it to the list of plots to save
-    cat(next.plot.name, " ", sort.diss[,1], "\n") #Print out the chosen plot and its dissimilarity score
+    if(verbose) cat(next.plot.name, " ", sort.diss[,1], "\n") #Print out the chosen plot and its dissimilarity score
     speciesVsitesMatrix_binary[start.plot,] <- speciesVsitesMatrix_binary[start.plot,] + speciesVsitesMatrix_binary[next.plot.name,] #Merge the seed plot with the latest selected plot to get all occurrences into one virtual plot
     speciesVsitesMatrix_binary[start.plot,][speciesVsitesMatrix_binary[start.plot,] > 0] <- 1 #set all values to 1 so it is PA data in case species are shared
     speciesVsitesMatrix_binary <- speciesVsitesMatrix_binary[(!rownames(speciesVsitesMatrix_binary) %in% next.plot.name),]
@@ -164,13 +164,13 @@ simpson_beta.opt <- function(speciesVsitesMatrix_binary, n.plt, start, plot_name
 
 
 ########################
-Frequent_simpson_beta.opt <- function(speciesVsitesMatrix_binary, n.plt, iterations) {
+Frequent_simpson_beta.opt <- function(speciesVsitesMatrix_binary, n.plt, iterations, verbose) {
     opt.runs.freq <- list()
     n <- 0
     for(i in 1:iterations) {
       n <- n + 1
-      cat("Rep ", n, "\n")
-      opt.runs.freq[[n]] <- simpson_beta.opt(speciesVsitesMatrix_binary, n.plt, start = "random")
+      if(verbose) cat("Rep ", n, "\n")
+      opt.runs.freq[[n]] <- simpson_beta.opt(speciesVsitesMatrix_binary, n.plt, start = "random", verbose)
       } #end iterations
     freq_plots <- plyr::count(unlist(lapply(opt.runs.freq, FUN=function(x) as.character(names(x$richness)))))
     freq_plots <- freq_plots[rev(order(freq_plots$freq)),]
@@ -192,12 +192,12 @@ Frequent_simpson_beta.opt <- function(speciesVsitesMatrix_binary, n.plt, iterati
   
   
 ####################################
-Random.opt <- function(speciesVsitesMatrix_binary, n.plt, iterations) {
+Random.opt <- function(speciesVsitesMatrix_binary, n.plt, iterations, verbose) {
     Sppaccum_freq <- list() #
     n <- 0
     for(i in 1:iterations) {
       n <- n + 1
-      cat("Rep ", n, "\n")
+      if(verbose) cat("Rep ", n, "\n")
       Sppaccum_freq[[n]]<- specaccum(speciesVsitesMatrix_binary[sample(nrow(speciesVsitesMatrix_binary), n.plt),], method="collector")
       }  
     combined.rand_specaccum <- Sppaccum_freq[[1]] #copy one for format
