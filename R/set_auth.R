ec255e4feebee372cf11ab42dc162b758dc67473
Greg Guerin <GregGuerin@users.noreply.github.com>
2020-10-28 13:37:53 +1030

    Update set_auth.R
    Exchange message for print so text on console can be more easily suppressed (CRAN requirement)

--- R/set_auth.R
+++ R/set_auth.R
@@ -3,12 +3,12 @@ set_auth <- function(role=NULL, secret=NULL) {
   if(is.null(secret)) stop("secret must be the secret used to authenticate you to the API")
   options("ausplotsR_role" = role)
   options("ausplotsR_secret" = secret)
-  print("auth set successfully (but credentials not yet tested), all queries will now include unpublished visits")
+  message("auth set successfully (but credentials not yet tested), all queries will now include unpublished visits")
 }
 
 
 unset_auth <- function() {
   options("ausplotsR_role" = NULL)
   options("ausplotsR_secret" = NULL)
-  print("auth un-set successfully, you're now the same as everyone else (no unpublished visits for you)")
+  message("auth un-set successfully, you're now the same as everyone else (no unpublished visits for you)")
 }
