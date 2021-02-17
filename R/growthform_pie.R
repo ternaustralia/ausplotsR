bb3df2126df5dd2f6402e957b094137149fe2407
Greg Guerin <GregGuerin@users.noreply.github.com>
2021-02-17 15:28:17 +1030

    Update growthform_pie.R
    When filtering out growth forms with 0 cover before plotting pie chart, change from removing those that are 0 to including all that are > 0. This is because if all growth forms in the growth form table are present, '-which' does not run.
    
    This solves the problem of some ausplots crashing when attempting to run ausplots_visual on just that plot.

--- R/growthform_pie.R
+++ R/growthform_pie.R
@@ -1,7 +1,7 @@
 growthform_pie <- function(GF, colours) {
 	
-	GF <- GF[,-(which(colSums(GF) == 0))]
-	
+    GF <- GF[, which(colSums(GF) > 0)]
+
 	pie(x=as.numeric(GF), labels=round(as.numeric(GF), digits=0), main=row.names(GF), col=colours[names(GF)])
 	try(legend("right", legend=names(GF), pch=20, col=colours[names(GF)], bty="n", cex=0.7, pt.cex=1))
 	
