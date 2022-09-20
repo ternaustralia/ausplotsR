d0a79e1a9645d3e6c620cd5c7a60385e1dfff62b
Greg Guerin <GregGuerin@users.noreply.github.com>
2022-09-20 16:31:03 +0930

    Update ausplots_visual.R
    Update automated data extraction (where no data imputed) to correctly extract veg,PI given the table is no longer returned by default in get_ausplots

--- R/ausplots_visual.R
+++ R/ausplots_visual.R
@@ -32,8 +32,8 @@ ausplots_visual <- function(my.ausplots.object = NULL, map = TRUE, map.attribute
 	#check data and extract if missing
 	if(missing(my.ausplots.object)) {
 		message("Obtaining sample data...") #combine two calls to get all sites but only veg.PI for that bounding box so make it quicker.
-		my.ausplots.object <- get_ausplots(veg.vouchers=FALSE, veg.PI=FALSE) #get all sites
-		my.ausplots.object$veg.PI <- get_ausplots(my.Plot_IDs=sample(my.ausplots.object$site.info$site_location_name, max.plots), site_info=FALSE, veg.vouchers=FALSE)$veg.PI #get veg PI for random plots  - according to max.plots
+		my.ausplots.object <- get_ausplots() #get all sites, site info only
+		my.ausplots.object$veg.PI <- get_ausplots(my.Plot_IDs=sample(my.ausplots.object$site.info$site_location_name, max.plots), site_info=FALSE, veg.PI=TRUE)$veg.PI #get veg PI for random plots  - according to max.plots
 	}
 	
 	###############################################
