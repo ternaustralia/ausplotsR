a8877511d62d9d9769cf54a9cbb3a575d9ce1eaa
Greg Guerin <GregGuerin@users.noreply.github.com>
2022-09-28 12:07:45 +0930

    Update whitt.plot.R
    More try wrappers in plotting to avoid crashing iterating over plots

--- R/whitt.plot.R
+++ R/whitt.plot.R
@@ -1,4 +1,4 @@
 whitt.plot <- function(abund_vect) {
-	plot(vegan::rad.lognormal(round(abund_vect, digits=0), log="y"), pch=20, main=rownames(abund_vect), bty="l")
-	legend("topright", lwd=3, legend=c("Lognormal"), bty="n")                       
+	try(plot(vegan::rad.lognormal(round(abund_vect, digits=0), log="y"), pch=20, main=rownames(abund_vect), bty="l"))
+	try(legend("topright", lwd=3, legend=c("Lognormal"), bty="n"))                       
 }
