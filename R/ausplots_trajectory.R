6b10e187baefe935109cad2c0dee9a3c282851ef
Greg Guerin <GregGuerin@users.noreply.github.com>
2023-03-30 16:56:28 +1030

    Update ausplots_trajectory.R
    Revisions bringing functionality to producing trajectory PCoA plots

--- R/ausplots_trajectory.R
+++ R/ausplots_trajectory.R
@@ -1,39 +1,81 @@
-ausplots_trajectory <- function(my.ausplots.object, choices=c("PCoA", "diversity"), min.revisits=3) {
+ausplots_trajectory <- function(my.ausplots.object, choices=c("PCoA", "diversity"), min.revisits=3, plot_select=NULL) {
   
 #work in progress - not intended for public use as of 3/2023
 
+
+  #################################################
+  #check data specified
+  if(missing(my.ausplots.object)) {
+    stop("my.ausplots.object must be specified, see get_ausplots")
+  }
+  ###############################################
+  
+  
 #library(ecotraj)
 #library(vegan)
 
 #remove sites with less than min.revisits
-my.ausplots.object$site.info <- subset(my.ausplots.object$site.info, site_location_name %in% subset(my.ausplots.object$site.info, visit_number>=3)$site_location_name)
-
+my.ausplots.object$site.info <- subset(my.ausplots.object$site.info, site_location_name %in% subset(my.ausplots.object$site.info, visit_number>=min.revisits)$site_location_name)
+print(dim(my.ausplots.object$site.info))
 #occurrence atrix
-diss <- species_table(my.ausplots.object$veg.PI, m_kind="percent_cover", cover_type="PFC", species_name="HD", strip_bryophytes=TRUE)
+diss <- species_table(my.ausplots.object$veg.PI, m_kind="percent_cover", cover_type="PFC", species_name="SN")
+print(dim(diss))
 
 #remove sites not in revisited sites table
 diss <- diss[which(rownames(diss) %in% my.ausplots.object$site.info$site_unique),]
+print(dim(diss))
+diss <- diss[,colSums(diss) > 0]
+print(dim(diss))
 
 #remove sites not in veg data
 my.ausplots.object$site.info <- my.ausplots.object$site.info[which(my.ausplots.object$site.info$site_unique %in% rownames(diss)),]
+print(dim(my.ausplots.object$site.info))
 
 
-#dissimilarity
-dissdiss <- vegdist(diss)
+if("PCoA" %in% choices) {
+  
+  #dissimilarity
+  dissdiss <- vegan::vegdist(diss)
+  print(length(dissdiss))
+  
+if(is.null(plot_select)) {
+  ecotraj::trajectoryPCoA(dissdiss, sites=my.ausplots.object$site.info$site_location_name, 
+  surveys=my.ausplots.object$site.info$visit_number, selection=NULL, axes = c(1, 2), 
+  survey.labels=TRUE, lwd=1.5, lty=1, angle=15, length=0.2, 
+  traj.colors=rainbow(length(unique(my.ausplots.object$site.info$site_location_name))))
+} #end not select
 
-ecotraj::trajectoryPCoA(dissdiss, sites=meep$site.info$site_location_name, 
-surveys=meep$site.info$visit_number, selection=NULL, axes = c(1, 2), 
-survey.labels=TRUE, lwd=1.5, lty=1, angle=15, length=0.2, 
-traj.colors=rainbow(length(unique(meep$site.info$site_location_name))))
+#OR - selected sites,
+if(!is.null(plot_select)) {
+  ecotraj::trajectoryPlot(dissdiss, sites=my.ausplots.object$site.info$site_location_name, 
+  surveys=my.ausplots.object$site.info$visit_number, selection=my.ausplots.object$site.info$site_location_name %in% plot_select, axes = c(1, 2), 
+  survey.labels=TRUE, lwd=1.5, lty=1, angle=15, length=0.2, 
+  traj.colors=rainbow(length(unique(my.ausplots.object$site.info$site_location_name))))
+} #end plot select
+  
 
-#OR - selected sites, e.g.
+} #end if PCoA
 
+if("diversity" %in% choices) {
+  
+  DIV <- data.frame(shannon=vegan::diversity(diss), richness=vegan::diversity(diss, index="simpson"))
+  print(DIV)
+  if(is.null(plot_select)) {
+    ecotraj::trajectoryPlot(DIV, sites=my.ausplots.object$site.info$site_location_name, 
+                            surveys=my.ausplots.object$site.info$visit_number, selection=NULL, axes = c(1, 2), 
+                            survey.labels=TRUE, lwd=1.5, lty=1, angle=15, length=0.2, 
+                            traj.colors=rainbow(length(unique(my.ausplots.object$site.info$site_location_name))))
+  } #end not select
+  
+  #OR - selected sites,
+  if(!is.null(plot_select)) {
+    ecotraj::trajectoryPCoA(DIV, sites=my.ausplots.object$site.info$site_location_name, 
+                            surveys=my.ausplots.object$site.info$visit_number, selection=plot_select, axes = c(1, 2), 
+                            survey.labels=TRUE, lwd=1.5, lty=1, angle=15, length=0.2, 
+                            traj.colors=rainbow(length(unique(my.ausplots.object$site.info$site_location_name))))
+  } #end plot select
+  
 
-ecotraj::trajectoryPCoA(dissdiss, sites=meep$site.info$site_location_name, 
-               surveys=meep$site.info$visit_number, selection=c("SAAFLB0030", "SAAFLB0003", "SAAFLB0031"), axes = c(1, 2), 
-               survey.labels=TRUE, lwd=1.5, lty=1, angle=15, length=0.2, 
-               traj.colors=rainbow(length(unique(meep$site.info$site_location_name))))
-
-
+} #end if diversity
 
 } #end function
