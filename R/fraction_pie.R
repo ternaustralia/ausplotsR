740294b1a7db3aa68ac97d745a512a623ad8b539
Greg Guerin <GregGuerin@users.noreply.github.com>
2023-05-19 12:28:59 +0930

    Update fraction_pie.R
    update non-assigned fraction column to other from .NA in pie chart to match changes to fractional_cover_output

--- R/fraction_pie.R
+++ R/fraction_pie.R
@@ -1,5 +1,5 @@
 fraction_pie <- function(frac, row) {
-	try(pie(x=as.numeric(frac[row,c(2:ncol(frac))]), labels=as.numeric(frac[row,c("bare", "brown", "green", "NA.")[1:(ncol(frac)-1)]]), main=row, col=c("brown", "orange", "darkgreen", "gray")))
+	try(pie(x=as.numeric(frac[row,c(2:ncol(frac))]), labels=as.numeric(frac[row,c("bare", "brown", "green", "other")[1:(ncol(frac)-1)]]), main=row, col=c("brown", "orange", "darkgreen", "gray")))
 	
 	try(legend("topleft", legend=c("Bare ground", "Non-photosynthetic vegetation", "Photosynthetic vegetation", "NA"), pch=20, col=c("brown", "orange", "darkgreen", "gray"), bty="n", cex=0.8, pt.cex=1))
 }
